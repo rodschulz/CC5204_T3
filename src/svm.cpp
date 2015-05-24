290eb9bf299a3b909e4d7b18c4c8f48e8544a808
Francisca <faconcha@dcc.uchile.cl>
2015-05-23 21:24:43 -0400

    Starting svm

---
+++ src/svm.cpp
@@ -0,0 +1,10 @@
+//
+// Created by fran on 5/20/15.
+//
+
+#include "svm.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/ml/ml.hpp>
+
+using namespace cv;
---
+++ src/svm.h
@@ -0,0 +1,10 @@
+//
+// Created by fran on 5/20/15.
+//
+
+#ifndef CC5204_T3_SVM_H
+#define CC5204_T3_SVM_H
+
+#endif //CC5204_T3_SVM_H
+
+
